9919aa95d3080b4de32a7ee523f751cf3356f6d9
Nicholas Ferguson <nicholasferguson@wingarch.com>
2021-04-22 16:49:02 -0400

    initial

---
+++ plotPrism.py
@@ -0,0 +1,190 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
+
+def new_subplot(self):
+                nsubplots = len(self.__subplots) + 1
+
+                for i, subplot in enumerate(self.__subplots):
+                        subplot.change_geometry(nsubplots, 1, i + 1)
+
+                subplot = self.figure.add_subplot(nsubplots, 1, nsubplots)
+                subplot.grid(True)
+
+                self.__subplots.append(subplot)
+                self.__subplot = subplot
+
+def plot_prism(prism_definition1, prism_definition2,prism_definition3,title):
+    prism_definition_array1 = [
+        np.array(list(item))
+        for item in prism_definition1
+    ]
+    prism_definition_array2 = [
+        np.array(list(item))
+        for item in prism_definition2
+    ]
+    prism_definition_array3 = [
+        np.array(list(item))
+        for item in prism_definition3
+    ]
+    pt1x = (prism_definition_array1[1][0])
+    pt1y = (prism_definition_array1[2][1])
+    pt1z = (prism_definition_array1[3][2])
+
+    pt2x = (prism_definition_array2[1][0])
+    pt2y = (prism_definition_array2[2][1])
+    pt2z = (prism_definition_array2[3][2])
+
+    pt3x = (prism_definition_array3[1][0])
+    pt3y = (prism_definition_array3[2][1])
+    pt3z = (prism_definition_array3[3][2])
+#================================
+    points1 = []
+    points1 += prism_definition_array1
+    vectors1 = [
+        prism_definition_array1[1] - prism_definition_array1[0],
+        prism_definition_array1[2] - prism_definition_array1[0],
+        prism_definition_array1[3] - prism_definition_array1[0]
+    ]
+
+    points1 += [prism_definition_array1[0] + vectors1[0] + vectors1[1]]
+    points1 += [prism_definition_array1[0] + vectors1[0] + vectors1[2]]
+    points1 += [prism_definition_array1[0] + vectors1[1] + vectors1[2]]
+    points1 += [prism_definition_array1[0] + vectors1[0] + vectors1[1] + vectors1[2]]
+
+    points1 = np.array(points1)
+
+    edges1 = [
+        [points1[0], points1[3], points1[5], points1[1]],
+        [points1[1], points1[5], points1[7], points1[4]],
+        [points1[4], points1[2], points1[6], points1[7]],
+        [points1[2], points1[6], points1[3], points1[0]],
+        [points1[0], points1[2], points1[4], points1[1]],
+        [points1[3], points1[6], points1[7], points1[5]]
+    ]
+
+#================================
+    points2 = []
+    points2 += prism_definition_array2
+    vectors2 = [
+        prism_definition_array2[1] - prism_definition_array2[0],
+        prism_definition_array2[2] - prism_definition_array2[0],
+        prism_definition_array2[3] - prism_definition_array2[0]
+    ]
+
+    points2 += [prism_definition_array2[0] + vectors2[0] + vectors2[1]]
+    points2 += [prism_definition_array2[0] + vectors2[0] + vectors2[2]]
+    points2 += [prism_definition_array2[0] + vectors2[1] + vectors2[2]]
+    points2 += [prism_definition_array2[0] + vectors2[0] + vectors2[1] + vectors2[2]]
+
+    points2 = np.array(points2)
+
+    edges2 = [
+        [points2[0], points2[3], points2[5], points2[1]],
+        [points2[1], points2[5], points2[7], points2[4]],
+        [points2[4], points2[2], points2[6], points2[7]],
+        [points2[2], points2[6], points2[3], points2[0]],
+        [points2[0], points2[2], points2[4], points2[1]],
+        [points2[3], points2[6], points2[7], points2[5]]
+    ]
+
+#================================
+    points3 = []
+    points3 += prism_definition_array3
+    vectors3 = [
+        prism_definition_array3[1] - prism_definition_array3[0],
+        prism_definition_array3[2] - prism_definition_array3[0],
+        prism_definition_array3[3] - prism_definition_array3[0]
+    ]
+
+    points3 += [prism_definition_array3[0] + vectors3[0] + vectors3[1]]
+    points3 += [prism_definition_array3[0] + vectors3[0] + vectors3[2]]
+    points3 += [prism_definition_array3[0] + vectors3[1] + vectors3[2]]
+    points3 += [prism_definition_array3[0] + vectors3[0] + vectors3[1] + vectors3[2]]
+
+    points3 = np.array(points3)
+    edges3 = [
+        [points3[0], points3[3], points3[5], points3[1]],
+        [points3[1], points3[5], points3[7], points3[4]],
+        [points3[4], points3[2], points3[6], points3[7]],
+        [points3[2], points3[6], points3[3], points3[0]],
+        [points3[0], points3[2], points3[4], points3[1]],
+        [points3[3], points3[6], points3[7], points3[5]]
+    ]
+#===============================
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    faces1 = Poly3DCollection(edges1, linewidths=1, edgecolors='k')
+    faces1.set_facecolor((0,1,1,0.1))  #greem dot
+    ax.add_collection3d(faces1)
+
+    faces2 = Poly3DCollection(edges2, linewidths=1, edgecolors='k')
+    faces2.set_facecolor((0,0,1,0.1)) #blue  dot
+    ax.add_collection3d(faces2)
+
+    faces3 = Poly3DCollection(edges3, linewidths=1, edgecolors='k')
+    faces3.set_facecolor(((0.8, 0.6, 0.5,0.1))) # red  dot
+    ax.add_collection3d(faces3)
+
+    labelW='test'
+    if title == 'a_Right_b_Eqn03':
+        labelW='a->b'
+        labelA ='a=A'
+        labelB ='b=d_dr'
+    elif title == 'b_Left_a_Eqn04':
+        labelW='b<-a'
+        labelA ='a=A'
+        labelB ='b=d_dr'
+    elif title == 'symmetric_Eqn05':
+        labelW = '1/2[a->b + b<-a] {exp}'
+        labelA ='a=A'
+        labelB ='b=d_dr'
+    elif title == 'antisymmetric_Eqn06':
+        labelW = '1/2[a->b - b<-a] [exp]'
+        labelA ='a=A'
+        labelB ='b=d_dr'
+    elif title == 'E Eqn: 09':
+        labelW = '  -1* 1/2[a->b + b<-a] {exp}'
+        labelA ='a=A'
+        labelB ='b=d_dr'
+    elif title == 'B Eqn: 10':
+        labelW = '1/2[a->b - b<-a] [exp]'
+        labelA ='a=A'
+        labelB ='b=d_dr'
+    elif title =='[d/dr,B]==+{d/dr,E}':
+        labelW = '[d/dr,B]->(+{d/dr,E})'
+        labelA ='antisymmetric(a,B(a,d_dr))'
+        labelB ='symmetric(d_dr,E(a,d_dr))'
+
+
+
+    ax.plot(pt1x, pt1y, pt1z, markerfacecolor='g', markeredgecolor='k', marker='o', markersize=10, alpha=0.6,label=labelA)
+    ax.plot(pt3x, pt3y, pt3z, markerfacecolor='r', markeredgecolor='k', marker='o', markersize=10, alpha=0.6,label=labelB)
+    ax.plot(pt2x, pt2y, pt2z, markerfacecolor='b', markeredgecolor='k', marker='o', markersize=10, alpha=0.6,label=labelW)
+   # maxX = max(pt1x,pt2x,pt3x)
+   # maxY= max(pt1y,pt2y,pt3y)
+   # maxY= max(pt1z,pt2z,pt3z)
+    maxA = max(abs(pt1x),abs(pt2x),abs(pt3x),abs(pt1y),abs(pt2y),abs(pt3y),abs(pt1z),abs(pt2z),abs(pt3z))
+
+    # Plot the points themselves to force the scaling of the axes
+  #  ax.scatter(points1[:,0], points1[:,1], points1[:,2], s=1)
+
+ #   ax.set_aspect('auto')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    # Setting the axes properties
+    maxA = maxA+2
+    ax.set_xlim3d([-maxA, maxA])
+    ax.set_ylim3d([-maxA, maxA])
+    ax.set_zlim3d([-maxA, maxA])
+  #  return ax
+    plt.title(title)
+    plt.legend(loc='upper right')
+
+    plt.show()
+
+
---
+++ qEM.py
@@ -0,0 +1,302 @@
+
+import math
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import interactive
+from plotPrism import plot_prism  #plotprism.py
+
+
+# Fill in a start prism wire frame, using point and a 3D origin as vertices
+# Highlight point as vertex
+def displayResults(pq,pq2,rq,title):
+        p1 = np.array([0.,0.,0.])
+        p2 = np.array([pq[1],0.,0.])
+        p3 = np.array([0.,pq[2],0.])
+        p4 = np.array([0.,0.,pq[3]])
+
+        prism1 = [
+            (p1[0],p1[1],p1[2]), (p2[0],p2[1],p2[2]), (p3[0],p3[1],p3[2]), (p4[0],p4[1],p4[2])
+        ]
+
+# Fill in a rotated prism wire frame, using point and a 3D origin as vertices
+# Highlight point as vertex
+
+        p1A = np.array([0.,0.,0.])
+        p2A = np.array([pq2[1],0.,0.])
+        p3A = np.array([0.,pq2[2],0.])
+        p4A = np.array([0.,0.,pq2[3]])
+
+        prism2 = [
+            (p1A[0],p1A[1],p1A[2]), (p2A[0],p2A[1],p2A[2]), (p3A[0],p3A[1],p3A[2]), (p4A[0],p4A[1],p4A[2])
+        ]
+
+        p1B = np.array([0.,0.,0.])
+        p2B = np.array([rq[1],0.,0.])
+        p3B = np.array([0.,rq[2],0.])
+        p4B = np.array([0.,0.,rq[3]])
+
+        prism3 = [
+            (p1B[0],p1B[1],p1B[2]), (p2B[0],p2B[1],p2B[2]), (p3B[0],p3B[1],p3B[2]), (p4B[0],p4B[1],p4B[2])
+       ]
+
+# to prove that plot is correct, you need to rotate image along its axes.
+
+        plot_prism(prism1,prism2,prism3,title)
+
+
+def normalize(v, tolerance=0.00001):
+    mag2 = sum(n * n for n in v)
+    if abs(mag2 - 1.0) > tolerance:
+        mag = math.sqrt(mag2)
+        v = tuple(n / mag for n in v)
+    return np.array(v)
+
+# a->b
+def a_Right_b_Eqn03(a,b):
+    return [a[0]*b[0]-a[1]*b[1]-a[2]*b[2]-a[3]*b[3],
+            a[0]*b[1]+a[1]*b[0]+a[2]*b[3]-a[3]*b[2],
+            a[0]*b[2]-a[1]*b[3]+a[2]*b[0]+a[3]*b[1],
+            a[0]*b[3]+a[1]*b[2]-a[2]*b[1]+a[3]*b[0]]
+
+# b <- a
+def b_Left_a_Eqn04(b,a):
+    return [a[0]*b[0]-a[1]*b[1]-a[2]*b[2]-a[3]*b[3],
+            a[0]*b[1]+a[1]*b[0]-a[2]*b[3]+a[3]*b[2],
+            a[0]*b[2]+a[1]*b[3]+a[2]*b[0]-a[3]*b[1],
+            a[0]*b[3]-a[1]*b[2]+a[2]*b[1]+a[3]*b[0]]
+
+# 1/2[a->b + b<-a] {}
+
+def symmetric_Eqn05(a,b):
+    A = a
+    dr = b
+    R = a_Right_b_Eqn03(a,b)
+    L = b_Left_a_Eqn04(b,a)
+    return [.5*(R[0]+L[0]),
+            .5*(R[1]+L[1]),
+            .5*(R[2]+L[2]),
+            .5*(R[3]+L[3])]
+
+# 1/2[a->b - b<-a] []
+def antisymmetric_Eqn06(a,b):
+    A = a
+    dr = b
+    R = a_Right_b_Eqn03(A,dr)
+    L = b_Left_a_Eqn04(dr,A)
+    Z = [.5*(R[0]-L[0]),
+            .5*(R[1]-L[1]),
+            .5*(R[2]-L[2]),
+            .5*(R[3]-L[3])]
+  #  if Z[0] == 0:
+  #      Z[0] = 1
+    return Z
+
+# electric field is the negative symmetric
+# derivative of the potential
+# eqn 09
+def fE_Eqn09(a,b):
+    # r = normalize(rq)
+     A=a
+     dr=b
+     R = a_Right_b_Eqn03(dr,A)
+     L = b_Left_a_Eqn04(A,dr)
+     return [-1*.5*(R[0]+L[0]),
+             -1*.5*(R[1]+L[1]),
+             -1*.5*(R[2]+L[2]),
+             -1*.5*(R[3]+L[3])]
+# magnetic field is the positive antisymmetric_Eqn06
+# derivative of the potential
+def fB_Eqn10(a,b):
+    #r = normalize(rq)
+     A=a
+     dr = b
+     R = a_Right_b_Eqn03(dr,A)
+     L = b_Left_a_Eqn04(A,dr)
+     return [.5*(R[0]-L[0]),
+             .5*(R[1]-L[1]),
+             .5*(R[2]-L[2]),
+             .5*(R[3]-L[3])]
+# a is pure quaternion
+# rq is rotator quaternion
+
+def build_prism_with_Right(a,b):
+    #r = normalize(rq)
+    #r_conj = [r[0],-1*r[1],-1*r[2],-1*r[3]]
+    #return a_Right_b_Eqn03(a_Right_b_Eqn03(r,a),r_conj)
+    return a_Right_b_Eqn03(a,b)
+
+
+
+def build_prism_with_Left(a,b):
+    #r = normalize(rq)
+    #r_conj = [r[0],-1*r[1],-1*r[2],-1*r[3]]
+    #return b_Left_a_Eqn04(b_Left_a_Eqn04(r,a),r_conj)
+    return b_Left_a_Eqn04(b,a)
+
+def build_prism_with_symmetric_Eqn05(a,b):
+   # r = normalize(rq)
+    #r_conj = [r[0],-1*r[1],-1*r[2],-1*r[3]]
+    #return symmetric_Eqn05(symmetric_Eqn05(r,a),r_conj)
+    return symmetric_Eqn05(a,b)
+
+def build_prism_with_antisymmetric_Eqn06(a,b):
+    #r = normalize(rq)
+    #r_conj = [r[0],-1*r[1],-1*r[2],-1*r[3]]
+    # return antisymmetric_Eqn06(antisymmetric_Eqn06(r,a),r_conj)
+    return antisymmetric_Eqn06(a,b)
+
+def build_prism_with_B_eqn10(a,b):
+    #r = normalize(rq)
+    #r_conj = [r[0],-1*r[1],-1*r[2],-1*r[3]]
+    # return B(B(r,a),r_conj)
+    return fB_Eqn10(a,b)
+
+def build_prism_with_E_eqn09(a,b):
+   # r = normalize(rq)
+    #r_conj = [r[0],-1*r[1],-1*r[2],-1*r[3]]
+    #return E(E(r,a),r_conj)
+    return fE_Eqn09(a,b)
+
+def Equation13(a,b):
+    d_dr = b
+    A = a
+    BB=antisymmetric_Eqn06(d_dr,fB_Eqn10(A,d_dr))
+    EE=symmetric_Eqn05(d_dr,fE_Eqn09(A,d_dr))
+    print("Eqn 13: antisymmetric_Eqn06 [d/dr,B]",BB)
+    print("Eqn 13: symmetric_Eqn05 +{d/dr,E}",EE)
+    ret = [0,0,0,0]
+    ret[0] = BB[0] - EE[0]
+    ret[1] = BB[1] - EE[1]
+    ret[2] = BB[2] - EE[2]
+    ret[3] = BB[3] - EE[3]
+    #r = normalize(rq)
+    #a_conj = [a[0],-1*a[1],-1*a[2],-1*a[3]]
+    #b_conj = [b[0],-1*b[1],-1*b[2],-1*b[3]]
+    return BB,EE,ret
+    #return a_Right_b_Eqn03(a,b)
+
+def Equation14(a,b):
+    d_dr = b
+    A = a
+    BB=antisymmetric_Eqn06(d_dr,fE_Eqn09(A,d_dr))
+    EE=symmetric_Eqn05(d_dr,fB_Eqn10(A,d_dr))
+    print("Eqn 13: antisymmetric_Eqn06 [d/dr,B]",BB)
+    print("Eqn 13: symmetric_Eqn05 +{d/dr,E}",EE)
+    ret = [0,0,0,0]
+    ret[0] = BB[0] -  (-1*EE[0])
+    ret[1] = BB[1] -  (-1*EE[1])
+    ret[2] = BB[2] -  (-1*EE[2])
+    ret[3] = BB[3] -  (-1*EE[3])
+    #r = normalize(rq)
+    #a_conj = [a[0],-1*a[1],-1*a[2],-1*a[3]]
+    #b_conj = [b[0],-1*b[1],-1*b[2],-1*b[3]]
+    return BB,EE,ret
+    #return a_Right_b_Eqn03(a,b)
+
+def Equation22(a,b):
+    A = a
+    dr = b
+    e23=a_Right_b_Eqn03(dr,a_Right_b_Eqn03(dr,A))
+    e24=b_Left_a_Eqn04(b_Left_a_Eqn04(A,dr),dr)
+    print("eq22 e23",e23)
+    print("eq22 e24",e24)
+    e25=[0,0,0,0]
+    e25[0]=e23[0]-e24[0]
+    e25[1]=e23[1]-e24[1]
+    e25[2]=e23[2]-e24[2]
+    e25[3]=e23[3]-e24[3]
+
+    return e25;
+
+def Equation23(a,b):
+   # A = normalize(a)
+   # dr = normalize(b)
+    A = a
+    dr = b
+    e23=a_Right_b_Eqn03(dr,b_Left_a_Eqn04(A,dr))
+    print("eqn 23 e23",e23)
+    e24=b_Left_a_Eqn04(a_Right_b_Eqn03(dr,A),dr)
+    print("eqn 23 e24",e24)
+    e25=[0,0,0,0]
+    e25[0]=e23[0]-e24[0]
+    e25[1]=e23[1]-e24[1]
+    e25[2]=e23[2]-e24[2]
+    e25[3]=e23[3]-e24[3]
+
+    return e25;
+
+def Equation24(a,b):
+    A = a
+    dr = b
+    e23=a_Right_b_Eqn03(dr,a_Right_b_Eqn03(dr,A))
+    e24=b_Left_a_Eqn04(b_Left_a_Eqn04(A,dr),dr)
+    print("eq24 e23",e23)
+    print("eq24 e24",e24)
+    e25=[0,0,0,0]
+    e25[0]=e23[0]+e24[0]
+    e25[1]=e23[1]+e24[1]
+    e25[2]=e23[2]+e24[2]
+    e25[3]=e23[3]+e24[3]
+
+    return e25;
+def main():
+    degrees = math.pi/180;
+    rot = 180*degrees;
+    # for rotating about an axis.
+    w = math.cos(rot/2.);
+    ax = math.sin(rot/2.);
+    # quaternion format is [scalar, x, y ,z]
+    #  a = [0, 1, 2, 3]  # Electric potential.
+    A = [(w)*.5, (ax)*1, (ax)*2, (ax)*3]  # Electric potential.
+    d_dr = [-5*ax,w,w*2,w*3]  # d/dr.
+    interactive(True)
+
+    #r = normalize(rq)
+    AB = build_prism_with_Right(A,d_dr)
+    print("a_Right_b_Eqn03 AB",AB)
+    #print("a_Right_b_Eqn03 a",A)
+    #print("a_Right_b_Eqn03 b",d_dr)
+    displayResults(A,AB,d_dr,"a_Right_b_Eqn03")
+
+
+    BA = build_prism_with_Left(A,d_dr)
+    print("b_Left_a_Eqn04 ",BA)
+    displayResults(A,BA,d_dr,"b_Left_a_Eqn04")
+
+    aAB = build_prism_with_antisymmetric_Eqn06(A,d_dr)
+    print("antisymmetric_Eqn06 ",aAB)
+    displayResults(A,aAB,d_dr,"antisymmetric_Eqn06")
+
+    sAB = build_prism_with_symmetric_Eqn05(A,d_dr)
+    print("symmetric_Eqn05 ",sAB)
+    displayResults(A,sAB,d_dr,"symmetric_Eqn05")
+
+    B = build_prism_with_B_eqn10(A,d_dr)
+    print("B :",B)
+    displayResults(A,B,d_dr,"B Eqn: 10")
+
+
+    E = build_prism_with_E_eqn09(A,d_dr)
+    print("E :",E)
+    displayResults(A,E,d_dr,"E Eqn: 09")
+
+    BB,EE,ret = Equation13(A,d_dr)
+
+    interactive(False)
+
+    displayResults(BB,EE,ret,"[d/dr,B]==+{d/dr,E}")
+
+
+
+    print("Equation22 \n",Equation22(A,d_dr))
+
+    print("Equation23 \n",Equation23(A,d_dr))
+
+    print("Equation24 \n",Equation24(A,d_dr))
+
+
+
+if __name__ == "__main__":
+    main()
+
